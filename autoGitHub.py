a1654c9dcd2e958421f8ed7b4215ba3a90d60ae9
Emmett <jae35@ph-180617.uol.le.ac.uk>
2025-09-26 15:37:40 +0100

    adding docstrings, testing more variables, revised global min/max calculation

--- autoGitHub.py
+++ autoGitHub.py
@@ -1,10 +1,13 @@
 import os
 
-def push2GitHub(comment):
+def push2GitHub(message):
+
+    """Automatically commit and push all workspace files to a GitHub repository, with a commit message.
+    """
 
     os.system("git remote -v")
     os.system("git add .")
-    os.system(f'git commit -m "{comment}"')
+    os.system(f'git commit -m "{message}"')
     os.system("git push -u origin main")
 
-push2GitHub('adding docstrings')
+push2GitHub('adding docstrings, testing more variables, revised global min/max calculation')
