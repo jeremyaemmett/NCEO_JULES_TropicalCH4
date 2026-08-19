b22fd83901b2f787e1efe2f96a0b9455fb9fa176
jeremyaemmett <jae35@leicester.ac.uk>
2026-08-19 18:44:58 +0100

    automatic looping of postprocessing scripts

--- output/jedi_scripts/utc.py
+++
@@ -1,4 +0,0 @@
-from datetime import datetime
-
-def utc(time: datetime) -> str:
-    return time.strftime('%Y-%m-%dT%H:%M:%SZ')
--- output/tools/misc.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-def createTime(t0, tf, deltat, discard):
-    t = np.arange(t0 + discard * deltat, tf + deltat / 2, deltat)
-    return t
